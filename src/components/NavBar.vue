a8c15fdbe73626b691f62dbb8bb6b4a9a004b9bc
pcyyura <cyp.yura@gmail.com>
2025-07-11 14:45:32 +0800

    feat: navbar layout and install tailwind

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav
+    class="flex justify-between items-center absolute w-full px-18 h-20 bg-primary"
+  >
+    <div>Yura</div>
+    <div class="flex items-center h-20 px-4 bg-white-soft">
+      <ul class="flex justify-between items-center gap-6">
+        <li>作品集</li>
+        <li>部落格</li>
+        <li>關於我</li>
+      </ul>
+    </div>
+  </nav>
+</template>
