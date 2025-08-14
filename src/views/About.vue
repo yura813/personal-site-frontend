36e647efbade07aca5cd876f200dc9c7fa17328b
pcyyura <cyp.yura@gmail.com>
2025-08-14 16:44:35 +0800

    feat: add about content (WIP)

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,47 +1,89 @@
 <script setup></script>
 
 <template>
-  <div class="flex flex-col gap-0">
+  <div>
     <div
       class="relative flex items-center justify-between top-20 px-40 h-120 font-sans bg-primary tracking-wider"
     >
       <div class="flex flex-col gap-20 text-white-soft">
-        <div class="flex flex-col gap-4 text-2xl">
+        <div class="flex flex-col gap-4 text-2xl font-bold">
           <p>Hi, I'm Yura</p>
           <p>Front-end Developer</p>
           <p>UI / UX Designer</p>
         </div>
         <div class="flex flex-col gap-4 text-lg">
-          <p class="flex gap-3">
-            <span># HTML</span>
-            <span># CSS</span>
-            <span># JavaScript</span>
-            <span># Vue</span>
+          <p class="flex gap-4">
+            <span> HTML</span>
+            <span> CSS</span>
+            <span> JavaScript</span>
+            <span> Vue</span>
           </p>
-          <p class="flex gap-3">
-            <span># Node.js</span>
-            <span># Express</span>
-            <span># Postgre SQL</span>
-            <span># Git</span>
+          <p class="flex gap-4">
+            <span> Node.js</span>
+            <span> Express</span>
+            <span> Postgre SQL</span>
+            <span> Git</span>
           </p>
-          <p class="flex gap-3">
-            <span># Figma</span>
-            <span># Survey Design</span>
-            <span># User Interview</span>
-            <span># Statistical Analysis</span>
+          <p class="flex gap-4">
+            <span> Figma</span>
+            <span> Survey Design</span>
+            <span> User Interview</span>
+            <span> Statistical Analysis</span>
           </p>
         </div>
       </div>
       <div class="w-64">
-        <img
-          class="w-full"
-          src="https://fakeimg.pl/300x200/C00CCC"
-          alt="這是一張圖片"
-        />
+        <img class="w-full" src="" alt="這是一張圖片" />
       </div>
     </div>
-    <div class="flex px-40 top-20 h-100 bg-purple-100">
-      <p class="font-bold text-2xl">Education</p>
+
+    <div class="bg-purple-100 min-h-screen flex flex-col gap-16 pt-40">
+      <div class="relative px-40 tracking-wide">
+        <div>
+          <p class="font-bold text-2xl mb-6 text-primary">About Me</p>
+          <div class="w-full flex justify-between">
+            <p>
+              Hi, I’m Yura — a listener, problem-solver, and curious thinker.
+              <br />
+              My interest in product design and the interaction between people
+              and technology began in university through design thinking and
+              UI/UX courses. Wanting to turn ideas into reality, I joined the
+              Front-End Application Development Bootcamp to learn front-end
+              skills, aiming to bridge design and technology.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="relative px-40 tracking-wide">
+        <div>
+          <p class="font-bold text-2xl mb-6 text-primary">Highlights</p>
+          <div class="flex flex-col gap-4">
+            <p>
+              ● Graduated from a four-month Front-End Bootcamp at the Commerce
+              Development Research Institute, building real-world projects.
+            </p>
+            <p>
+              ● Led an eight-member team project, managing feature assignments and
+              resolving collaboration challenges.
+            </p>
+            <p>
+              ● Former Product Manager at DSI Group, overseeing three SaaS
+              products and collaborating with engineers on UI, features, and
+              database structures.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="relative px-40 tracking-wide">
+        <div>
+          <p class="font-bold text-2xl mb-6 text-primary">Education</p>
+          <div class="w-full flex justify-between">
+            <p>National Taiwan University</p>
+            <span>2025.03 - 2025.07</span>
+            <p></p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
