89b38ece39dbf8f78c599d69e83825cf72ce0079
pcyyura <cyp.yura@gmail.com>
2025-08-24 18:47:58 +0800

    feat: add menu animation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import { AlignJustify, ChevronUp } from "lucide-vue-next";
+import { AlignJustify, X } from "lucide-vue-next";
 const router = useRouter();
 const isMenuOpen = ref(false);
 const goTo = (path) => {
   router.push(path);
   isMenuOpen.value = false;
-}
+};
 const navItems = [
   { name: "Portfolio", path: "/portfolio" },
   { name: "Blog", path: "/blog" },
@@ -26,7 +26,7 @@ const navItems = [
     </div>
     <div class="block md:hidden" @click="isMenuOpen = !isMenuOpen">
       <AlignJustify class="text-white-soft" v-if="!isMenuOpen" />
-      <ChevronUp class="text-white-soft" v-if="isMenuOpen" />
+      <X class="text-white-soft" v-if="isMenuOpen" />
     </div>
     <div class="hidden md:flex items-center h-20 bg-white-soft">
       <div class="flex justify-between items-center">
@@ -51,17 +51,18 @@ const navItems = [
     </div>
   </nav>
   <div
-    class="h-screen w-full fixed top-20 bottom-0 right-0 bg-white-soft md:hidden py-10"
-    v-if="isMenuOpen"
+    v-show="true"
+    class="h-screen w-full fixed top-20 bottom-0 right-0 bg-white-soft md:hidden py-10 transition-all duration-300 ease-out transform"
+    :class="
+      isMenuOpen
+        ? 'opacity-100'
+        : 'opacity-0 pointer-events-none'
+    "
   >
     <div
       class="flex flex-col gap-10 px-10 text-xl text-primary underline-offset-6 underline"
     >
-      <p
-        v-for="item in navItems"
-        :key="item.name"
-        @click="goTo(item.path)"
-      >
+      <p v-for="item in navItems" :key="item.name" @click="goTo(item.path)">
         {{ item.name }}
       </p>
     </div>
